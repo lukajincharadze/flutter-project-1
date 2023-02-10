78005b3e3a6b41f25f94b9722a1d608b71db9d54
Luka Jincharadze <lukajinchara@gmail.com>
2023-02-11 00:51:49 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), // ThemeData
+      home: Scaffold (
+        appBar: AppBar (title: Text("Cubes"),),
+        body: Container(
+            child: Column (
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                  alignment: AlignmentDirectional.bottomEnd,
+                )
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
